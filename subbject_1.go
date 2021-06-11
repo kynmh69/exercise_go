7d197f135d8eabf0d9f0385e47c5419063df07f1
kynmh69 <hiroki.kayanuma@gmail.com>
2021-06-11 10:41:42 +0900

    subject 1

--- subbject_1.go
+++ subbject_1.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	i := 1
-	fmt.Println(i)
+	// Q1
+	i := 1.11
+	fmt.Println(int(i))
+
+	// Q2
+	// [5, 6]
+
+	// Q3
+	m := map[string]int{
+		"Mike":  20,
+		"Nancy": 24,
+		"Messi": 30,
+	}
+	fmt.Printf("%T %v", m, m)
 }
