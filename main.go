5c97bc383b987f417fb6f7b28150ff7eb75deee5
kynmh69 <hiroki.kayanuma@gmail.com>
2021-06-18 19:01:40 +0900

    create main

--- main.go
+++ main.go
@@ -1,42 +1,9 @@
 package main
 
-import "fmt"
-
-type Vertex struct {
-	x, y int
-}
-
-func (v Vertex) Area() int {
-	return v.x * v.y
-}
-
-func (v *Vertex) Scale(i int) {
-	v.x *= i
-	v.y *= i
-}
-
-func New(x, y, z int) *Vertex3D {
-	return &Vertex3D{Vertex{x, y}, z}
-}
-
-type Vertex3D struct {
-	Vertex
-	z int
-}
-
-func (v Vertex3D) Area3D() int {
-	return v.x * v.y * v.z
-
-}
-
-func (v *Vertex3D) Scale3D(i int) {
-	v.x *= i
-	v.y *= i
-	v.z *= i
+type Human interface {
+	Say()
 }
 
 func main() {
-	v := New(3, 4, 5)
-	v.Scale3D(10)
-	fmt.Printf("Area %d", v.Area3D())
+
 }
